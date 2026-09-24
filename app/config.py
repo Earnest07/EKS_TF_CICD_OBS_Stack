411f83102421979aa3f8e9e5efcb85fe1ad676f3
Earnest07 <ebenezerearnest01.com>
2026-09-24 18:17:14 +0530

    Jenkins start pipeline

--- app/config.py
+++ app/config.py
@@ -1,3 +1,18 @@
-APP_NAME = "EKS CI/CD Demo Application"
-APP_VERSION = "v1.0.0"
-APP_ENVIRONMENT = "local"
+
+import os
+
+APP_NAME = os.getenv(
+    "APP_NAME",
+    "EKS CI/CD Demo Application",
+)
+
+APP_VERSION = os.getenv(
+    "APP_VERSION",
+    "v1.0.0",
+)
+
+APP_ENVIRONMENT = os.getenv(
+    "APP_ENVIRONMENT",
+    "local",
+)
+
